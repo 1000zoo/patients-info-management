4e6f8c62e8492f234cf966e89dd9cfba3f493a0b
1000zoo <cjswldn99@gmail.com>
2022-11-28 13:54:08 +0900

    Camera 와의 의존관계 수정

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:ediya/barcode.dart';
+import 'package:ediya/camera_test.dart';
 import 'package:ediya/config.dart';
 import 'package:ediya/sp_helper.dart';
 import 'package:ediya/util.dart';
@@ -11,8 +13,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class CameraPage extends StatefulWidget {
   final SPHelper helper;
+  final CameraDescription camera;
 
-  const CameraPage(this.helper, {super.key});
+  const CameraPage(this.helper, this.camera, {super.key});
 
   @override
   State<CameraPage> createState() => _CameraPageState();
@@ -30,16 +33,15 @@ class _CameraPageState extends State<CameraPage> {
       ),
       child: ListView(
         children: [
-          SizedBox(height: MediaQuery.of(context).size.width / 12),
+          SizedBox(height: MediaQuery.of(context).size.height / 12),
           CupertinoButton(
             padding: BUTTON_PADDING,
             onPressed: () async {
               barcode = await Navigator.push(
-                context,
-                CupertinoPageRoute(
-                  builder: (context) => Barcode(widget.helper),
-                )
-              );
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) => Barcode(widget.helper),
+                  ));
               detected = barcode != "";
             },
             child: const Icon(Icons.search),
@@ -47,43 +49,41 @@ class _CameraPageState extends State<CameraPage> {
           SizedBox(height: MediaQuery.of(context).size.width / 12),
           CupertinoButton(
             padding: BUTTON_PADDING,
-            onPressed: ()  {
+            onPressed: () {
               if (!detected) {
                 getAlertDialog(context, "바코드를 스캔하세요!");
-              } else if (!widget.helper.contains(barcode)){
-                getAlertDialog(context, "등록되지 않은 환자입니다!\n등록 먼저 하시거나 바코드를 다시 스캔해주세요.");
+              } else if (!widget.helper.contains(barcode)) {
+                getAlertDialog(
+                    context, "등록되지 않은 환자입니다!\n등록 먼저 하시거나 바코드를 다시 스캔해주세요.");
               } else {
                 showCupertinoDialog(
-                  context: context,
-                  builder: (context) {
-                    return CupertinoAlertDialog(
-                      title: Text("${widget.helper.getPersonName(barcode)} 님이 맞습니까?"),
-                      actions: [
-                        CupertinoButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text("아니요"),
-                        ),
-                        CupertinoButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                builder: (context) => const Camera()
-                              )
-                            ).then(Navigator.of(context).pop).then((value) {
-                              setState(() {
-
-                              });
-                            });
-                          },
-                          child: const Text("예")
-                        )
-                      ],
-                    );
-                  }
-                );
+                    context: context,
+                    builder: (context) {
+                      return CupertinoAlertDialog(
+                        title: Text(
+                            "${widget.helper.getPersonName(barcode)} 님이 맞습니까?"),
+                        actions: [
+                          CupertinoButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("아니요"),
+                          ),
+                          CupertinoButton(
+                              onPressed: () {
+                                Navigator.push(
+                                        context,
+                                        CupertinoPageRoute(
+                                            builder: (context) => Camera(widget.helper, barcode)))
+                                    .then(Navigator.of(context).pop)
+                                    .then((value) {
+                                  setState(() {});
+                                });
+                              },
+                              child: const Text("예"))
+                        ],
+                      );
+                    });
               }
             },
             child: const Icon(Icons.camera),
@@ -93,4 +93,3 @@ class _CameraPageState extends State<CameraPage> {
     );
   }
 }
-
