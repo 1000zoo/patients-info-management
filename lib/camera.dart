c90cb2f85b2a751c6445d3ea0b1ebcfb5dd37cec
1000zoo <cjswldn99@gmail.com>
2022-11-30 11:04:54 +0900

    사진 저장 전, 기분 선택, 바코드-기분-날짜 로 저장

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:cupertino_radio_choice/cupertino_radio_choice.dart';
+import 'package:ediya/MoodListPage.dart';
 import 'package:ediya/config.dart';
 import 'package:ediya/util.dart';
 import 'package:flutter/material.dart';
@@ -11,10 +13,10 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
 import 'sp_helper.dart';
 
-
 class Camera extends StatefulWidget {
   final SPHelper helper;
   final String barcode;
+
   const Camera(this.helper, this.barcode, {Key? key}) : super(key: key);
 
   @override
@@ -23,30 +25,38 @@ class Camera extends StatefulWidget {
 
 class _CameraState extends State<Camera> {
   File? _image;
+  XFile? tempImage;
   final picker = ImagePicker();
-  // Person? person;
+  String mood = "";
+  bool moodCheck = false;
 
   @override
   void initState() {
-    // person = widget.helper.getPersonInfo(widget.barcode);
     super.initState();
   }
 
   // 비동기 처리를 통해 카메라와 갤러리에서 이미지를 가져온다.
   Future getImage(ImageSource imageSource) async {
-    final image = await picker.pickImage(source: imageSource);
-    String dir = (await getApplicationDocumentsDirectory()).path;
-    String newPath = join(dir, imagePath());
-    _image = await File(image!.path).copy(newPath); // 가져온 이미지를 _image 에 저장
-
-    setState(() {
-
-    });
+    tempImage = await picker.pickImage(
+        source: imageSource, preferredCameraDevice: CameraDevice.front);
+    setState(() {});
   }
 
   String imagePath() {
-    var newFileName = "${widget.barcode}-${nowString()}.png";
+    var newFileName = "${widget.barcode}-$mood-${nowString()}.png";
     return newFileName;
+  }
+
+  Future<void> changeImagePath() async {
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    String newPath = join(dir, imagePath());
+
+    File temp =
+        await File(tempImage!.path).copy(newPath); // 가져온 이미지를 _image 에 저장
+    setState(() {
+      _image = temp;
+    });
+    print(_image!.path);
   }
 
   // 이미지를 보여주는 위젯
@@ -54,11 +64,11 @@ class _CameraState extends State<Camera> {
     return Container(
         color: const Color(0xffd0cece),
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 1.2,
         child: Center(
-            child: _image == null
+            child: tempImage == null
                 ? const Text('No image selected.', style: TEXT_STYLE)
-                : Image.file(File(_image!.path))));
+                : Image.file(File(tempImage!.path))));
   }
 
   @override
@@ -75,44 +85,91 @@ class _CameraState extends State<Camera> {
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               showImage(),
-              SizedBox(height: MediaQuery.of(context).size.height / 5),
-              if (_image == null) Row(
-                      children: [
-                        SizedBox(
-                            width: MediaQuery.of(context).size.width / 1.2),
-                        CupertinoButton(
-                          padding: BUTTON_PADDING,
-                          color: const Color(0x9D283FE5),
-                          onPressed: () {
-                            getImage(ImageSource.camera);
-                          },
-                          child:
-                              const Icon(Icons.camera_alt_outlined, size: 50),
-                        )
-                      ],
-                    ) else Row(
-                      children: [
-                        SizedBox(width: MediaQuery.of(context).size.width / 1.2),
-                        CupertinoButton(
-                          padding: BUTTON_PADDING,
-                          color: const Color(0x9D283FE5),
-                          onPressed: () {
-                            GallerySaver.saveImage(_image!.path).then((value) {
-                              if (value.toString() == 'true') {
-                                  getAlertDialog(context, "저장되었습니다.").then(
-                                  Navigator.of(context).pop
-                                );
-                              } else {
-                                  getAlertDialog(context, "알 수 없는 오류").then(
-                                  Navigator.of(context).pop
-                                );
-                              }
-                            });
-                          },
-                          child: const Text("저장", style: TEXT_STYLE,),
-                        )
-                      ],
+              // SizedBox(height: MediaQuery.of(context).size.height / 5),
+              if (tempImage == null)
+                Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 1.2),
+                    CupertinoButton(
+                      padding: BUTTON_PADDING,
+                      color: const Color(0x9D283FE5),
+                      onPressed: () {
+                        getImage(ImageSource.camera);
+                      },
+                      child: const Icon(Icons.camera_alt_outlined, size: 50),
                     )
+                  ],
+                )
+              else
+                Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 20),
+                    CupertinoRadioChoice(
+                      choices: const {'좋음': '좋음', '그럭저럭': '그럭저럭', '나쁨': '나쁨'},
+                      onChange: (selected) {
+                        setState(() {
+                          mood = selected;
+                          moodCheck = true;
+                        });
+                      },
+                      initialKeyValue: '그럭저럭',
+                      selectedColor: const Color(0x9D283FE5),
+                    ),
+                    SizedBox(width: MediaQuery.of(context).size.width / 2.1),
+                    CupertinoButton(
+                      padding: BUTTON_PADDING,
+                      color: const Color(0x9D283FE5),
+                      onPressed: () {
+                        if (!moodCheck) {
+                          getAlertDialog(context, "기분을 선택하세요!");
+                        } else {
+                          showCupertinoDialog(
+                              context: context,
+                              builder: (context) {
+                                return CupertinoAlertDialog(
+                                  title: Text("'$mood'을 선택하신 것이 맞습니까?"),
+                                  actions: [
+                                    CupertinoButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("아니요"),
+                                    ),
+                                    CupertinoButton(
+                                        onPressed: () async {
+                                          await changeImagePath(); //비동기여서 에러가 생겼었음
+                                          GallerySaver.saveImage(_image!.path, albumName: widget.helper.getPersonName(widget.barcode))
+                                              .then((value) {
+                                            if (value.toString() == 'true') {
+                                              getAlertDialog(
+                                                      context, "저장되었습니다.")
+                                                  .then(Navigator.of(context)
+                                                      .pop).then(Navigator.of(context).pop);
+                                            } else {
+                                              getAlertDialog(
+                                                      context, "알 수 없는 오류")
+                                                  .then(Navigator.of(context)
+                                                      .pop);
+                                            }
+                                          });
+                                        },
+                                        child: const Text("예"))
+                                  ],
+                                );
+                              });
+                        }
+                      },
+                      child: const Text(
+                        "저장",
+                        style: TEXT_STYLE,
+                      ),
+                    )
+                  ],
+                )
             ]));
   }
 }
+
+//getMoodListTile(Mood.good),
+// getMoodListTile(Mood.notBad),
+// getMoodListTile(Mood.bad),
